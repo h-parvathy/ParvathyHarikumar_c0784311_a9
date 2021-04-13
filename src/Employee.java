ff89c2c4e09d84420f500f3d9080f6a5947690ff
h-parvathy <76555469+h-parvathy@users.noreply.github.com>
2021-04-12 22:12:14 -0400

    Exception added

--- src/Employee.java
+++ src/Employee.java
@@ -5,19 +5,23 @@ public class Employee {
         JFrame input;
         input=new JFrame();
 
-        int id = Integer.parseInt(JOptionPane.showInputDialog(input, "Enter Employee ID:"));
-        String name = JOptionPane.showInputDialog(input, "Enter Employee Username:");
-        double sales = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter sales made this month"));
-        double advance = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter advance taken this month:"));
+        try{
+            int id = Integer.parseInt(JOptionPane.showInputDialog(input, "Enter Employee ID:"));
+            String name = JOptionPane.showInputDialog(input, "Enter Employee Username:");
+            double sales = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter sales made this month"));
+            double advance = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter advance taken this month:"));
+            CommissionCalculator calculator = new CommissionCalculator(id,name,sales,advance);
+            calculator.calcPay();
 
-        CommissionCalculator calculator = new CommissionCalculator(id,name,sales,advance);
-        calculator.calcPay();
+            if (!(calculator.getPay()<0)) {
+                JOptionPane.showMessageDialog(input,"Salary of "+name + " is "+ calculator.getPay());
+            } else
+                JOptionPane.showMessageDialog(input,name + " need to reimburse "+ calculator.getPay()+
+                        "$ to the company");
 
-        if (!(calculator.getPay()<0)) {
-            JOptionPane.showMessageDialog(input,"Salary of "+name + " is "+ calculator.getPay());
-        } else
-            JOptionPane.showMessageDialog(input,name + " need to reimburse "+ calculator.getPay()+
-                    "$ to the company");
+        }catch (Exception e){
+            System.out.println(e);
+        }
 
     }
 }
